a328c950115f5e9aa1a6c4fbb19214777bbc5e2c
吳偉雄 <137890151+lchenglin29@users.noreply.github.com>
2024-06-16 07:07:02 +0000

    註解

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from flask import Flask,request
+from flask import Flask,request #引入需要的函式庫與模組
 import os
 from firebase import firebase
 
 
-url = os.environ['url']
-def get_db_data(id):
+url = os.environ['url'] # 從環境變數取得我的資料庫url
+def get_db_data(id):    # 取得資料的函式
   fdb = firebase.FirebaseApplication(url, None)
   result = fdb.get('/',id)
   if result == None:
     return "Error"
   else:
     return result
-def set_db_data(id:str,data):
+def set_db_data(id:str,data):  # 寫入資料的函式
   fdb = firebase.FirebaseApplication(url, None)
   fdb.put('/rank',id,data)
 
@@ -20,7 +20,7 @@ app = Flask(__name__)
 def main():
     return "Hello!"
   
-@app.route("/name", methods=["GET"])
+@app.route("/name", methods=["GET"]) # 寫入資料的路徑
 def name():
     name = request.args.get('name')
     score = request.args.get('score')
@@ -28,7 +28,7 @@ def name():
     print(score)
     return name
 
-@app.route("/rank", methods=["GET"])
+@app.route("/rank", methods=["GET"]) # 取得排列過後資料的路徑
 def rank():
     data = get_db_data("rank")
     sorted_ids = sorted(data.keys(), key=lambda x: data[x],reverse=True)
@@ -40,6 +40,5 @@ def rank():
       except Exception as e:
         print(e)
     return lb
-
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3000)
+  app.run(host='0.0.0.0', port=3000) # 運行伺服器
